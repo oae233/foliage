2a9d6d7c00d020358b4c6826b219369ea8e12dab
Omar ElGamal <61190251+oae233@users.noreply.github.com>
2024-02-07 21:41:14 +0400

    first code
    This is only a div that displays the uploaded image

---
+++ script.js
@@ -0,0 +1,10 @@
+const dropArea= document.getElementById("drop-area");
+const inputFile= document.getElementById("input-file");
+const imageView= document.getElementById("img-view");
+
+inputFile.addEventListener("change", uploadImage);
+
+function uploadImage(){
+    let imgLink = URL.createObjectURL(inputFile.files[0]);
+    imageView.style.backgroundImage = `url(${imgLink})`;
+}
